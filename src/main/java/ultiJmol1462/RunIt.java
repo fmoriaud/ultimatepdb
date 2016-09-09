e377f8637e93dddde662a31fe61e77a6438f995b
fmoriaud <fmoriaud@yahoo.fr>
2016-09-09 18:53:06 +0200

    tentative code to use Jmol14.6.2. Would need to make an artifact of myJmol 16.6.2 and use it in the libs instead of the other one

---
+++ src/main/java/ultiJmol1462/RunIt.java
@@ -0,0 +1,26 @@
+package ultiJmol1462;
+
+import org.openscience.jmol.app.Jmol;
+import org.openscience.jmol.app.jmolpanel.JmolPanel;
+
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import static org.jmol.script.T.frame;
+
+/**
+ * Created by Fabrice.Moriaud on 08.09.2016.
+ */
+public class RunIt {
+
+    public static void main(String [] args) {
+        MyJmol myJmol = new MyJmol();
+
+
+    }
+    }
