adbd1353562d55400325d6cfff3aee83d6cbc509
Fabrice Moriaud <Fabrice.Moriaud@bruker.com>
2016-10-12 13:51:10 +0200

    Java class code template

---
+++ .idea/fileTemplates/internal/Class.java
@@ -0,0 +1,62 @@
+/*
+Copyright 2016 Fabrice Moriaud <fmoriaud@ultimatepdb.org>
+
+
+This file is part of ultimatepdb.
+
+		ultimatepdb is free software: you can redistribute it and/or modify
+		it under the terms of the GNU Lesser General Public License as published by
+		the Free Software Foundation, either version 3 of the License, or
+		(at your option) any later version.
+
+		ultimatepdb is distributed in the hope that it will be useful,
+		but WITHOUT ANY WARRANTY; without even the implied warranty of
+		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+		GNU Lesser General Public License for more details.
+
+		You should have received a copy of the GNU Lesser General Public License
+		along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+public class ${NAME} {
+}
+//-------------------------------------------------------------
+// Enum
+//-------------------------------------------------------------
+
+
+
+//-------------------------------------------------------------
+// Class members
+//-------------------------------------------------------------
+
+
+
+//-------------------------------------------------------------
+// Constructor
+//-------------------------------------------------------------
+
+
+
+//-------------------------------------------------------------
+// Public & Override methods
+//-------------------------------------------------------------
+
+
+
+//-------------------------------------------------------------
+// Implementation
+//-------------------------------------------------------------
+
+
+
+//-------------------------------------------------------------
+// Inner classes
+//-------------------------------------------------------------
+
+
+
+//-------------------------------------------------------------
+// Getters & Setters
+//-------------------------------------------------------------
