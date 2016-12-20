82dd844fc84d31a7e61faa6087e008c322db39b4
Fabrice Moriaud <fmoriaud@yahoo.fr>
2016-12-20 20:27:54 +0100

    more Test. Looks ok.

--- src/test/java/shapeReduction/ShapeReductorByClusteringTest.java
+++ src/test/java/shapeReduction/ShapeReductorByClusteringTest.java
@@ -118,4 +118,116 @@ public class ShapeReductorByClusteringTest {
         assertTrue(propertiesMiniPoint1.size() == 1);
         assertTrue(propertiesMiniPoint1.get(0) == StrikingProperties.HYDROPHOBE);
     }
+
+
+    @Test
+    public void testHbondAcceptorAndFormalNegativechargeInLineInGrid() throws IOException, ParsingConfigFileException {
+
+        AlgoParameters algoParameters = Tools.generateModifiedAlgoParametersForTestWithTestFolders();
+
+        float criticalDistance = algoParameters.getCELL_DIMENSION_OF_THE_PROBABILITY_MAP_ANGSTROM() * (float) Math.sqrt(3) + 0.1f;
+        float cellSize = algoParameters.getCELL_DIMENSION_OF_THE_PROBABILITY_MAP_ANGSTROM();
+
+        List<PointWithPropertiesIfc> listPointsWithProperties = new ArrayList<>();
+        // cretae points
+        for (int i = 0; i < 10; i++) {
+            PointWithPropertiesIfc point = new PointWithProperties();
+            float[] coords = new float[3];
+            coords[0] = (float) i * cellSize;
+            coords[1] = 0;
+            coords[2] = 0;
+            point.setCoords(new Point(coords));
+
+            List<StrikingProperties> properties = new ArrayList<>();
+            properties.add(StrikingProperties.HBOND_ACCEPTOR);
+            if (i < 3){
+                properties.add(StrikingProperties.NEGATIVE_CHARGE);
+            }
+            point.setStrikingProperties(properties);
+
+            listPointsWithProperties.add(point);
+        }
+
+        CollectionOfPointsWithPropertiesIfc points = new CollectionOfPointsWithProperties(listPointsWithProperties);
+
+        ShapeReductorByClustering shapeReductorByClustering = new ShapeReductorByClustering(points, algoParameters);
+
+        Map<Integer, PointWithPropertiesIfc> minishape = shapeReductorByClustering.computeReducedCollectionOfPointsWithProperties();
+
+        // That works nicely
+        // H is HbondAcceptor and A is Negative Charge
+        // H H H H H H H H H
+        // A A A H H H H H H
+        //   H           H
+        //   A
+        assertTrue(minishape.size() == 2);
+        PointWithPropertiesIfc miniPoint2 = minishape.get(1);
+        List<StrikingProperties> propertiesMiniPoint0 = miniPoint2.getStrikingProperties();
+        assertTrue(propertiesMiniPoint0.size() == 2);
+        assertTrue(propertiesMiniPoint0.contains(StrikingProperties.HBOND_ACCEPTOR));
+        assertTrue(propertiesMiniPoint0.contains(StrikingProperties.NEGATIVE_CHARGE));
+
+        PointWithPropertiesIfc miniPoint1 = minishape.get(7);
+        List<StrikingProperties> propertiesMiniPoint1 = miniPoint1.getStrikingProperties();
+        assertTrue(propertiesMiniPoint1.size() == 1);
+        assertTrue(propertiesMiniPoint1.get(0) == StrikingProperties.HBOND_ACCEPTOR);
+    }
+
+
+    @Test
+    public void testHbondAcceptorAHBaondDonnorndFormalNegativechargeInLineInGrid() throws IOException, ParsingConfigFileException {
+
+        AlgoParameters algoParameters = Tools.generateModifiedAlgoParametersForTestWithTestFolders();
+
+        float criticalDistance = algoParameters.getCELL_DIMENSION_OF_THE_PROBABILITY_MAP_ANGSTROM() * (float) Math.sqrt(3) + 0.1f;
+        float cellSize = algoParameters.getCELL_DIMENSION_OF_THE_PROBABILITY_MAP_ANGSTROM();
+
+        List<PointWithPropertiesIfc> listPointsWithProperties = new ArrayList<>();
+        // cretae points
+        for (int i = 0; i < 10; i++) {
+            PointWithPropertiesIfc point = new PointWithProperties();
+            float[] coords = new float[3];
+            coords[0] = (float) i * cellSize;
+            coords[1] = 0;
+            coords[2] = 0;
+            point.setCoords(new Point(coords));
+
+            List<StrikingProperties> properties = new ArrayList<>();
+            properties.add(StrikingProperties.HBOND_ACCEPTOR);
+            if (i < 3){
+                properties.add(StrikingProperties.NEGATIVE_CHARGE);
+            }
+            if (i > 4){
+                properties.add(StrikingProperties.HBOND_DONNOR);
+            }
+            point.setStrikingProperties(properties);
+
+            listPointsWithProperties.add(point);
+        }
+
+        CollectionOfPointsWithPropertiesIfc points = new CollectionOfPointsWithProperties(listPointsWithProperties);
+
+        ShapeReductorByClustering shapeReductorByClustering = new ShapeReductorByClustering(points, algoParameters);
+
+        Map<Integer, PointWithPropertiesIfc> minishape = shapeReductorByClustering.computeReducedCollectionOfPointsWithProperties();
+
+        // That works nicely
+        // H is HbondAcceptor and A is Negative Charge and D is HBOND Donnor
+        // H H H H H H H H H
+        // A A A H H D D D D
+        //   H           H
+        //   A           D
+        assertTrue(minishape.size() == 2);
+        PointWithPropertiesIfc miniPoint2 = minishape.get(1);
+        List<StrikingProperties> propertiesMiniPoint0 = miniPoint2.getStrikingProperties();
+        assertTrue(propertiesMiniPoint0.size() == 2);
+        assertTrue(propertiesMiniPoint0.contains(StrikingProperties.HBOND_ACCEPTOR));
+        assertTrue(propertiesMiniPoint0.contains(StrikingProperties.NEGATIVE_CHARGE));
+
+        PointWithPropertiesIfc miniPoint1 = minishape.get(7);
+        List<StrikingProperties> propertiesMiniPoint1 = miniPoint1.getStrikingProperties();
+        assertTrue(propertiesMiniPoint1.size() == 2);
+        assertTrue(propertiesMiniPoint1.get(0) == StrikingProperties.HBOND_ACCEPTOR);
+        assertTrue(propertiesMiniPoint1.get(1) == StrikingProperties.HBOND_DONNOR);
+    }
 }
